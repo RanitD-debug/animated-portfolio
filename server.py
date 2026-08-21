0808dc9196b52a6a559f6ab9c00ae406b4aafa66
Ranit Dutta <tapandutta0301@gmail.com>
2026-08-21 17:21:08 +0530

    feat: add 3 new testimonials

--- server.py
+++ server.py
@@ -8,6 +8,6 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
 if __name__ == '__main__':
-    with http.server.ThreadingHTTPServer(('127.0.0.1', 8080), Handler) as httpd:
+    with http.server.ThreadingHTTPServer(('0.0.0.0', 8080), Handler) as httpd:
         print("Server running at http://localhost:8080", flush=True)
         httpd.serve_forever()
